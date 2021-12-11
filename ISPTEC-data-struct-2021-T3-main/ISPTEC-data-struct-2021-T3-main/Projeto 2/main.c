e7251810499660e3d56c8bb0ce5a723292966c82
Kuenda Love <kuendalove14@gmail.com>
2021-12-11 09:43:04 +0000

    Update main.c

--- ISPTEC-data-struct-2021-T3-main/ISPTEC-data-struct-2021-T3-main/Projeto 2/main.c
+++ ISPTEC-data-struct-2021-T3-main/ISPTEC-data-struct-2021-T3-main/Projeto 2/main.c
@@ -1,32 +1,30 @@
 #include "gm.c"
 
 main()
-{
-	//setlocale(LC_ALL,"Portuguese");
-	
+{	
 	LCD * newList = _init();
 	char * name;
 	int i, n;
 	
-	printf("Quantos elementos deseja inserir na lista: ");
+	printf("Quantos elementos desejas inserir na lista: ");
 	scanf("%i", &n);
 	
 	for(i = 0; i< n; i++)
 	{
 		fflush(stdin);
+		printf("Nome do Soldado (%d): ", i+1);
 		gets(name);
 		fflush(stdin);
 		newList = _insertInStart(newList, name);
 	}
 	
+	printf("\n");
 	_print(newList, newList);
 	
 	newList = _remove(newList);
 	
 	printf("\n");
-	
-	_print(newList, newList);
-	
+	printf("Escolhido: %s", newList->name);
 	
 
 	
